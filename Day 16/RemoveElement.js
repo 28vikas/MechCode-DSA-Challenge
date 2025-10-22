c98f62d05e82f61dc26647698a7425a047a1f2b7
vikasstl <vikasgit@startelelogic.com>
2025-10-22 18:58:49 +0530

    Day 16

---
+++ Day 16/RemoveElement.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+let removeElement = function(nums,val){
+    let x = 0;
+    
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i] !== val){
+            nums[x] = nums[i];
+            x = x+1;
+        }
+    }
+
+    return x;
+}
+
+
+
+let nums = [3,2,2,3];
+let val = 3;
+let newLength = removeElement(nums, val);
+
+console.log(newLength); 
+console.log(nums);      
+
